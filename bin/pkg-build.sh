7d2e9be5d1feb330abae184c1343daf912559ecc
Nikita Klimin <nklimin007@gmail.com>
2022-09-19 12:51:26 +0300

    feat: finish pkg commands

---
+++ bin/pkg-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf package
+npx pkg -t ${TARGET_NODE:-node18}-macos-x64,${TARGET_NODE:-node18}-linux-x64,${TARGET_NODE:-node18}-win-x64 \
+    --compress GZip \
+    -o package/sqd \
+    package.json
