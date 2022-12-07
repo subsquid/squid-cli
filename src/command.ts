9b9031d3536fe179f2e3fafb0d8e7b5af8ea9f91
dzhelezov <dzhelezov@gmail.com>
2022-12-07 19:03:31 +0100

    add gravatar template and more detailed messages

--- src/command.ts
+++ src/command.ts
@@ -22,13 +22,13 @@ export abstract class CliCommand extends Command {
           }
           return this.error(body?.error || body.message);
         case 404:
-          return this.error(body?.error || body?.message || 'API url not found');
+          return this.error(`Unknown API endpoint. Check that your are using the latest version of the Squid CLI. Message: ${body?.error || body?.message || 'API url not found'}`);
 
         case 405:
           return this.error(body?.error || body?.message || 'Method not allowed');
         default:
           return this.error(
-            'Squid server error. Please come back later. If the error persists please open an issue at https://github.com/subsquid/squid and report to t.me/HydraDevs',
+            'Unknown server error. Please check that your are using the latest version of the Squid CLI. If the error persists please open an issue at https://github.com/subsquid/subsquid-cli and report to t.me/HydraDevs',
           );
       }
     }
