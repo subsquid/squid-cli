0d3b9914329465b558f1ea26036288e682858253
Eugene Formanenko <mo4islona@gmail.com>
2022-12-08 19:12:09 +0300

    fix: convert null to empty string

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,35 +4,34 @@ import path from 'path';
 import { CliUx, Flags } from '@oclif/core';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import yaml from 'js-yaml';
 import simpleGit from 'simple-git';
+import { adjectives, animals, colors, uniqueNamesGenerator } from 'unique-names-generator';
 
 import { squidNameIsAvailable } from '../api';
 import { CliCommand } from '../command';
-import { Manifest } from '../manifest';
-import { adjectives, animals, colors, uniqueNamesGenerator } from 'unique-names-generator';
+import { readManifest, saveManifest } from '../manifest';
 
 const SQUID_NAME_DESC = [
   `The squid name. It must contain only alphanumeric or dash ("-") symbols and must not start with "-".`,
   `Squid names are ${chalk.yellow('globally unique')}.`,
 ];
 
-const TEMPLATE_ALIASES: Record<string, { url: string, description: string }> = {
+const TEMPLATE_ALIASES: Record<string, { url: string; description: string }> = {
   evm: {
     url: 'https://github.com/subsquid/squid-evm-template',
-    description: `A minimal squid template for indexing EVM data.`
+    description: `A minimal squid template for indexing EVM data.`,
   },
   gravatar: {
     url: 'https://github.com/subsquid/gravatar-squid',
-    description: 'A sample EVM squid indexing the Gravatar smart contract on Ethereum.'
+    description: 'A sample EVM squid indexing the Gravatar smart contract on Ethereum.',
   },
   substrate: {
     url: 'https://github.com/subsquid/squid-substrate-template',
-    description: 'A template squid for indexing Substrate-based chains.'
+    description: 'A template squid for indexing Substrate-based chains.',
   },
   'frontier-evm': {
     url: 'https://github.com/subsquid/squid-frontier-evm-template',
-    description: 'A template for indexing Frontier EVM chains, like Moonbeam and Astar.'
+    description: 'A template for indexing Frontier EVM chains, like Moonbeam and Astar.',
   },
 };
 
@@ -47,7 +46,7 @@ const SQUID_TEMPLATE_DESC = [
     'squid.yaml',
   )} manifest in the root folder `,
   ` or one of the pre-defined aliases:`,
-  ...Object.entries(TEMPLATE_ALIASES).map(([alias, { url, description }]) => `     - ${chalk.bold(alias)}  ${description}`),
+  ...Object.entries(TEMPLATE_ALIASES).map(([alias, { description }]) => `     - ${chalk.bold(alias)}  ${description}`),
 ];
 
 export default class Init extends CliCommand {
@@ -113,9 +112,11 @@ export default class Init extends CliCommand {
         dictionaries: [adjectives, colors, animals],
         separator: '-',
         length: 2,
-      })
-      return this.error(`There is already a squid with name "${name}" deployed to Aquarium. Squid names are globally unique. ` +
-       `Please pick a new memorable name, e.g. "${uniqueNameSuggestion}".`);
+      });
+      return this.error(
+        `There is already a squid with name "${name}" deployed to Aquarium. Squid names are globally unique. ` +
+          `Please pick a new memorable name, e.g. "${uniqueNameSuggestion}".`,
+      );
     }
 
     if (fs.existsSync(localDir)) {
@@ -141,11 +142,12 @@ export default class Init extends CliCommand {
 
     const manifestPath = path.resolve(localDir, 'squid.yaml');
     try {
-      const manifestValue = yaml.load(fs.readFileSync(manifestPath).toString()) as Manifest;
+      const manifest = readManifest(manifestPath);
 
-      manifestValue.name = name;
+      /** Override name in squid manifest **/
+      manifest.name = name;
 
-      fs.writeFileSync(manifestPath, yaml.dump(manifestValue));
+      saveManifest(manifestPath, manifest);
     } catch (e: any) {
       return this.error(e);
     }
