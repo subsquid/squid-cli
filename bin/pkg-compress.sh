53629e02f6a7600a2e380e2a3e8ae836b4eb77cd
octo-gone <nklimin007@gmail.com>
2023-06-13 21:47:55 +0300

    fix: setup package.json for pkg

--- bin/pkg-compress.sh
+++ bin/pkg-compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd package
+cd dist
 mv sqd-macos sqd && tar -czf subsquid-cli-${npm_package_version}-macos-x64.tar.gz sqd && rm sqd
 mv sqd-linux sqd && tar -czf subsquid-cli-${npm_package_version}-linux-x64.tar.gz sqd && rm sqd
 mv sqd-win.exe sqd.exe
