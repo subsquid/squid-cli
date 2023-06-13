ac39a12a25eabf3dc688adb35863a34935918362
octo-gone <nklimin007@gmail.com>
2023-06-13 21:47:55 +0300

    fix: setup package.json for pkg

--- bin/pkg-build.sh
+++ bin/pkg-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf package
+rm -rf dist
 npx pkg -t ${TARGET_NODE:-node18}-macos-x64,${TARGET_NODE:-node18}-linux-x64,${TARGET_NODE:-node18}-win-x64 \
     --compress GZip \
-    -o package/sqd \
+    -o dist/sqd \
     package.json
