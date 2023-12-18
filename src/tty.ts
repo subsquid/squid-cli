17f2f371ee236f64715767d345f43eac0c9e59bf
Igor Mahov <tetya.a@yandex.ru>
2023-12-18 14:54:52 +0300

    fix: improve tty open

--- src/tty.ts
+++ src/tty.ts
@@ -1,18 +1,25 @@
-import assert from 'assert';
-import fs from 'fs';
+import { OpenMode, openSync } from 'fs';
 import tty from 'tty';
+
+function tryOpenSync(path: string, flags: OpenMode) {
+  try {
+    return openSync(path, flags);
+  } catch {
+    return undefined;
+  }
+}
 
 // https://github.com/postgres/postgres/blob/741fb0056eda5e7bd03deb0719121f88b4b9e34a/src/common/sprompt.c#L67
 function getTTY() {
   if (process.platform == 'win32') {
     return {
-      ttyFdIn: fs.openSync('CONIN$', 'w+'),
-      ttyFdOut: fs.openSync('CONOUT$', 'w+'),
+      ttyFdIn: tryOpenSync('CONIN$', 'w+'),
+      ttyFdOut: tryOpenSync('CONOUT$', 'w+'),
     };
   }
   return {
-    ttyFdIn: fs.existsSync('/dev/tty') ? fs.openSync('/dev/tty', 'r') : undefined,
-    ttyFdOut: fs.existsSync('/dev/tty') ? fs.openSync('/dev/tty', 'w') : undefined,
+    ttyFdIn: tryOpenSync('/dev/ttyF', 'r'),
+    ttyFdOut: tryOpenSync('/dev/ttyF', 'w'),
   };
 }
 
