fc6f39371938cf4f1e621f9e27adea582e5af6b4
Igor Mahov <tetya.a@yandex.ru>
2024-04-24 17:50:00 +0300

    feat: chain id search

--- src/commands/gateways/ls.ts
+++ src/commands/gateways/ls.ts
@@ -22,29 +22,34 @@ export default class Ls extends CliCommand {
       description: 'Search gateways',
       required: false,
     }),
+    id: Flags.string({
+      char: 'i',
+      description: 'Search by id',
+      required: false,
+    }),
   };
 
   async run(): Promise<void> {
     const {
-      flags: { type, search },
+      flags: { type, search, id: chainId },
     } = await this.parse(Ls);
 
     const [evm, substrate] = await Promise.all([
-      !type || type === 'evm' ? getEvmGateways() : [],
-      !type || type === 'substrate' ? getSubstrateGateways() : [],
+      !type || type === 'evm' || chainId ? getEvmGateways() : [],
+      !chainId && (!type || type === 'substrate') ? getSubstrateGateways() : [],
     ]);
 
     const maxNameLength = maxBy([...evm, ...substrate], (g) => g.chainName.length)?.chainName.length;
 
     switch (type) {
       case 'evm':
-        this.processGateways(evm, { search, summary: 'EVM', maxNameLength });
+        this.processGateways(evm, { search, summary: 'EVM', chainId, maxNameLength });
         break;
       case 'substrate':
         this.processGateways(substrate, { search, summary: 'Substrate', maxNameLength });
         break;
       default:
-        this.processGateways(evm, { search, summary: 'EVM', maxNameLength });
+        this.processGateways(evm, { search, summary: 'EVM', chainId, maxNameLength });
         this.log();
         this.processGateways(substrate, { search, summary: 'Substrate', maxNameLength });
     }
@@ -52,7 +57,12 @@ export default class Ls extends CliCommand {
 
   processGateways(
     gateways: Gateway[],
-    { summary, search, maxNameLength }: { search?: string; summary?: string; maxNameLength?: number },
+    {
+      search,
+      summary,
+      chainId,
+      maxNameLength,
+    }: { search?: string; summary?: string; chainId?: string; maxNameLength?: number },
   ) {
     if (summary) {
       this.log(chalk.bold(summary));
@@ -62,13 +72,14 @@ export default class Ls extends CliCommand {
       ? gateways.filter((g) => g.chainName.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
       : gateways;
 
+    if (summary === 'EVM' && chainId) gateways = gateways.filter((g) => String(g.chainId) === chainId);
+
     if (!gateways.length) {
       return this.log('No gateways found');
     }
 
     const headRow = ['Name', 'Release', 'Gateway URL'];
-    if (summary === 'EVM')
-      headRow.splice(1, 0, 'Chain ID');
+    if (summary === 'EVM') headRow.splice(1, 0, 'Chain ID');
     const table = new Table({
       wordWrap: true,
       colWidths: [maxNameLength ? maxNameLength + 2 : null],
@@ -84,8 +95,7 @@ export default class Ls extends CliCommand {
 
     gateways.map(({ chainName, chainId, providers }) => {
       const row = [chainName, chalk.dim(providers[0].release), providers[0].dataSourceUrl];
-      if (summary === 'EVM')
-        row.splice(1, 0, chalk.dim(chainId || ''));
+      if (summary === 'EVM') row.splice(1, 0, chalk.dim(chainId || ''));
       table.push(row);
     });
 
