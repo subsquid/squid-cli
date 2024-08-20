7ff7f6d591baa4c7470e08a7592f7f119e7b7f59
octo-gone <nklimin007@gmail.com>
2024-08-20 12:12:04 +0300

    fix: update cli to newest manifest

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { promisify } from 'util';
 
 import { Args, Flags, ux as CliUx } from '@oclif/core';
-import { ManifestValue } from '@subsquid/manifest';
+import { Manifest } from '@subsquid/manifest';
 import chalk from 'chalk';
 import { globSync } from 'glob';
 import ignore from 'ignore';
@@ -27,7 +27,7 @@ const SQUID_PATH_DESC = [
 export function resolveManifest(
   localPath: string,
   manifestPath: string,
-): { error: string } | { buildDir: string; squidDir: string; manifest: ManifestValue } {
+): { error: string } | { buildDir: string; squidDir: string; manifest: Manifest } {
   try {
     const { squidDir, manifest } = loadManifestFile(localPath, manifestPath);
 
